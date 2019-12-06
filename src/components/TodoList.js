bf660b89c01594534f03479fad10e2b35333ba00
Ben <ben.whitn@gmail.com>
2019-12-06 17:05:51 +0000

    asdasd

---
+++ src/components/TodoList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useContext } from "react";
+import { TodoContext } from "../contexts/TodoContext";
+import Todo from "./Todo";
+import { useTodos } from "../Hooks";
+import { AuthUserContext } from "../contexts/AuthUserContext";
+
+const TodoList = () => {
+  const { activeFilter } = useContext(TodoContext);
+  const authUser = useContext(AuthUserContext);
+
+  const { todos } = useTodos(authUser);
+
+  const filteredTodos = todos.filter(todo => {
+    switch (activeFilter) {
+      case "Active":
+        return !todo.completed;
+      case "Completed":
+        return todo.completed;
+      default:
+        return true;
+    }
+  });
+
+  return (
+    <React.Fragment>
+      {filteredTodos.map(todo => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
+    </React.Fragment>
+  );
+};
+
+export default TodoList;
